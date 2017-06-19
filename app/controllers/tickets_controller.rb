df71de10014445a1af01681f0378edb20e1d8bf7
Artur Okonski <okonski.a.dev@gmail.com>
2017-06-19 13:22:53 +0200

    Implement creating tickets for a project

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,43 @@
+class TicketsController < ApplicationController
+  before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_ticket
+
+  def show
+  end
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+
+    if @ticket.save
+      redirect_to [@project, @ticket], notice: 'Ticket has been created.'
+    else
+      flash.now[:alert] = 'Ticket has not been created.'
+      render :new
+    end
+  end
+
+  private
+
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+
+    def set_ticket
+      @ticket = @project.tickets.find(params[:id])
+    end
+
+    def invalid_ticket
+      logger.error "Attempt to access invalid ticket -> #{params[:id]}"
+      redirect_to @project,
+        alert: 'The ticket you were looking for could not be found.'
+    end
+
+    def ticket_params
+      params.require(:ticket).permit(:name, :description)
+    end
+end
