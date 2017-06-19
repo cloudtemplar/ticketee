110167d038c7d94a404ef18d89c072a5fc9f7e8d
Artur Okonski <okonski.a.dev@gmail.com>
2017-06-19 18:05:31 +0200

    Link tickets and users upon ticket creation

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
   it 'gets destroyed if its parent Project is destroyed' do
+    author = FactoryGirl.create(:user)
     project = FactoryGirl.create(:project)
-    FactoryGirl.create(:ticket, project: project)
+    FactoryGirl.create(:ticket, project: project, author: author)
 
     expect { project.destroy }.to change { Ticket.count }.by(-1)
   end
