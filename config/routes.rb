b24fb6c46b74e828a75098e534a37c357716d1fb
Artur Okonski <okonski.a.dev@gmail.com>
2017-06-20 09:04:19 +0200

    Add the ability to create users via admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
-
     resources :projects, only: [:new, :create, :destroy]
+    resources :users
   end
 
   resources :projects, except: [:new, :create, :destroy] do
