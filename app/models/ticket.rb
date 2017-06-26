e7ecdaf976bf7dfa611ff9d1c6511c2ab4599072
Artur Okonski <okonski.a.dev@gmail.com>
2017-06-26 15:18:31 +0200

    Tickets can have a state assigned when comments are created

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :project
   belongs_to :author, class_name: 'User'
+  belongs_to :state, optional: true
   has_many :attachments, dependent: :destroy, inverse_of: :ticket
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623115608) do
+ActiveRecord::Schema.define(version: 20170626124053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,9 @@ ActiveRecord::Schema.define(version: 20170623115608) do
     t.integer  "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "state_id"
     t.index ["author_id"], name: "index_comments_on_author_id", using: :btree
+    t.index ["state_id"], name: "index_comments_on_state_id", using: :btree
     t.index ["ticket_id"], name: "index_comments_on_ticket_id", using: :btree
   end
 
@@ -50,6 +52,11 @@ ActiveRecord::Schema.define(version: 20170623115608) do
     t.index ["user_id"], name: "index_roles_on_user_id", using: :btree
   end
 
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+  end
+
   create_table "tickets", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -57,8 +64,10 @@ ActiveRecord::Schema.define(version: 20170623115608) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "author_id"
+    t.integer  "state_id"
     t.index ["author_id"], name: "index_tickets_on_author_id", using: :btree
     t.index ["project_id"], name: "index_tickets_on_project_id", using: :btree
+    t.index ["state_id"], name: "index_tickets_on_state_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,10 +90,12 @@ ActiveRecord::Schema.define(version: 20170623115608) do
   end
 
   add_foreign_key "attachments", "tickets"
+  add_foreign_key "comments", "states"
   add_foreign_key "comments", "tickets"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "roles", "projects"
   add_foreign_key "roles", "users"
   add_foreign_key "tickets", "projects"
+  add_foreign_key "tickets", "states"
   add_foreign_key "tickets", "users", column: "author_id"
 end
